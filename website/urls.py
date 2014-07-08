ac64ec20271ed7f6185e705ac1a6c18b8cb62bd7
Joseph Seidel <seidel.jp@gmail.com>
2014-07-08 14:03:59 -0500

    The homepage has been created

--- website/urls.py
+++ website/urls.py
@@ -1,12 +1,18 @@
-from django.conf.urls import patterns, include, url
-
+from django.conf.urls import include, patterns, url
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
+from django.conf import settings
+
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'website.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+urlpatterns = i18n_patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('cms.urls')),
 )
+
+if settings.DEBUG:
+    urlpatterns = patterns('',
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+    url(r'', include('django.contrib.staticfiles.urls')),
+) + urlpatterns
