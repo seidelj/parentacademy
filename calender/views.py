709e491352cb2e6f75786f9e380b17a029605087
Joseph Seidel <seidel.jp@gmail.com>
2014-07-11 09:02:05 -0500

    Removed unnecessary templates

--- calender/views.py
+++ calender/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from calender.models import Events
 from django.http import Http404
+from datetime import date as day
 
 # Create your views here.
 def schedule(request, campus):
@@ -8,7 +9,7 @@ def schedule(request, campus):
 	if campus != "c" and campus != "m":
 		raise Http404
 	name = "Camden" if campus == "c" else "Middlesbrough"
-	events_list = Events.objects.filter(campus=campus).order_by('date')[:5]
+	events_list = Events.objects.filter(campus=campus).filter(date__gte=day.today).order_by('date')[:4]
 	noEvents = True if not events_list else False
 	context = { 'events': events_list, 'noEvents': noEvents, 'message': message, 'name': name, }
 	return render(request, 'calender/base.html', context)
