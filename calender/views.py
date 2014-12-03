7b83905c2bc9494363d205ae8469c4b41da499ef
Joseph Seidel <seidel.jp@gmail.com>
2014-12-03 08:40:59 -0600

    Added pagination

--- calender/views.py
+++ calender/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from calender.models import Events
 from django.http import Http404
 from datetime import date as day
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.template import RequestContext
 
 # Create your views here.
 def schedule(request, campus):
@@ -9,18 +11,38 @@ def schedule(request, campus):
 	if campus != "c" and campus != "m":
 		raise Http404
 	name = "Camden" if campus == "c" else "Middlesbrough"
-	events_list = Events.objects.filter(council=campus).filter(eventdate__gte=day.today).order_by('eventdate','start')[:10]
+	events_list = Events.objects.filter(council=campus).filter(eventdate__gte=day.today).order_by('eventdate','start')
 	noEvents = True if not events_list else False
-	context = { 'events': events_list, 'noEvents': noEvents, 'message': message, 'name': name, 'campus': campus, }
-	return render(request, 'calender/base.html', context)
+	paginator = Paginator(events_list, 10)
+	page = request.GET.get('page')
+	print page
+	try:
+		events = paginator.page(page)
+	except PageNotAnInteger:
+		events = paginator.page(1)
+	except EmptyPage:
+		events = paginator.page(paginator.num_pages)
+
+	context = { 'events': events, 'noEvents': noEvents, 'message': message, 'name': name, 'campus': campus, }
+	print page
+	return render_to_response('calender/base.html', context, context_instance=RequestContext(request))
 
 def schedule_group(request, campus, color):
 	message = "No upcoming event"
 	if campus != "c" and campus != "m":
 		raise Http404
 	name = "Camden" if campus == "c" else "MiddlesBrough"
-	events_list = Events.objects.filter(council=campus).filter(eventdate__gte=day.today).filter(color="#{}".format(color)).order_by('eventdate', 'start')[:10]
+	events_list = Events.objects.filter(council=campus).filter(eventdate__gte=day.today).filter(color="#{}".format(color)).order_by('eventdate', 'start')
 	noEvents = True if not events_list else False
-	context = { 'events': events_list, 'noEvents': noEvents, 'message': message, 'name': name, 'campus': campus }
-	return render(request, 'calender/base.html', context)
+	paginator = Paginator(events_list, 10)
+	page = request.GET.get('page')
+	try:
+		events = paginator.page(page)
+	except PageNotAnInteger:
+		events = paginator.page(1)
+	except EmptyPage:
+		events = paginator.page(paginator.num_pages)
+
+	context = { 'events': events, 'noEvents': noEvents, 'message': message, 'name': name, 'campus': campus }
+	return render_to_response('calender/base.html', context, context_instance=RequestContext(request))
 
